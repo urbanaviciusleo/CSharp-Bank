aa693573d8da14eb308db4e9426db367b740c527
Leonardo Urbanavicius <73943868+urbanaviciusleo@users.noreply.github.com>
2022-07-01 08:29:09 -0300

    C# FILES ADDED

---
+++ ContaCorrente.cs
@@ -0,0 +1,21 @@
+public class ContaCorrente 
+{
+    public string Titular;
+    public int Agencia;
+    public int Numero;
+    public double Saldo;
+
+    public ContaCorrente(string Titular, int Agencia, int Numero)
+    {
+        this.Titular = Titular;
+        this.Agencia = Agencia;
+        this.Numero = Numero;
+        Console.WriteLine("O titular da conta é " + this.Titular + ". A agência da conta é " + this.Agencia + ". O número da conta é " + this.Numero + ".");
+    }
+
+    public double deposita(double valor)
+    {
+        this.Saldo += valor;
+        return this.Saldo;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp
+{
+   class Program
+   {
+       static void Main(string[] args)
+       {
+          ContaCorrente CC = new ContaCorrente("Ana", 22, 22);
+          ContaCorrente CC1 = new ContaCorrente("Joao", 12, 12);
+          ContaCorrente CC2 = new ContaCorrente("Bruno", 42, 22);
+
+          CC.deposita(300);
+          
+       }
+   } 
+}
